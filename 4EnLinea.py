ab71ed99e3076318bd94dddc3c5c03fdde8049b3
Tomas Wardoloff <tomaswardoloff@gmail.com>
2021-04-08 17:29:12 -0300

    creacion de la funcion tiroValido

--- 4EnLinea.py
+++ 4EnLinea.py
@@ -12,11 +12,15 @@ def soltarfichaencolumna(ficha, columna, tablero):
         if tablero[row - 1][int(columna) - 1] == 0:
             tablero[row - 1][int(columna) - 1] = ficha
             return
+def tiroValido(columna):
+    if columna <= 7 & columna >= 0:
+        return 1
+    else:
+        return 0
 
 tablero = tablerovacio()
 for row in tablero:
     print(row)
-secuencia = [1, 2, 3, 1]
 
 for a in range(6, 0, -1):
     if a % 2 == 0:
@@ -24,6 +28,10 @@ for a in range(6, 0, -1):
     else:
         ficha=2
     columna = input("\nInsertar Numero de Columna: ")
-    soltarfichaencolumna(ficha, columna, tablero)
-    for row in tablero:
-        print(row)
+    if(tiroValido(columna)==1):
+        soltarfichaencolumna(ficha, columna, tablero)
+        for row in tablero:
+            print(row)
+    else:
+        print("Ese no es un tiro valido. El valor tiene que estar entre 0 y 7")
+        break
