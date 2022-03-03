a8e36df19d37341970c5fdf6b6a248b3821788f0
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2022-03-03 14:34:37 -0300

    First look main

---
+++ main.py
@@ -0,0 +1,21 @@
+from src.CuatroEnLinea import tiroValido
+from src.CuatroEnLinea import tableroVacio
+from src.CuatroEnLinea import dibujarTablero
+from src.CuatroEnLinea import completarTableroEnOrden
+
+def jugar(tablero, column, turno, gameOver):
+    if tiroValido(column):
+        completarTableroEnOrden(column, turno, tablero)
+        dibujarTablero(tablero)
+
+    gameOver = True
+
+if __name__ == "__main__":
+    gameOver = False
+    while not gameOver:
+        turno = 1
+        tablero = tableroVacio()
+
+        column = int(input("\nIngrese una fila: "))
+        
+        jugar(tablero, column, turno, gameOver)
